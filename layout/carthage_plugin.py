d36ce95b4294595e4db4920af23819d3267d4b3c
Sam Hartman <hartmans@debian.org>
2026-04-15 14:04:10 -0600

    Add support for a backend server that optionally serves the frontend if it is already built

--- layout/carthage_plugin.py
+++ layout/carthage_plugin.py
@@ -1,14 +1,10 @@
-# Copyright (C) 2026, Hadron Industries.
-# Carthage is free software; you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License version 3
-# as published by the Free Software Foundation. It is distributed
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the file
-# LICENSE for details.
 
 from carthage import inject, Injector
 from . import layout
+from .web_backend import start_web_server, web_server_key
 
 @inject(injector=Injector)
 def carthage_plugin(injector):
     injector.add_provider(layout.layout)
+    injector.add_provider(web_server_key, start_web_server)
+    
