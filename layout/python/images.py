c3a49575e9bf14aefcee7355d8473853c9f78862
TimRCM <Tim@TimRCM.com>
2026-04-23 15:39:54 -0600

    Add router to serve dhcp/dns for the mitigation lab

--- layout/python/images.py
+++ layout/python/images.py
@@ -1,29 +1,30 @@
-# Copyright (C) 2026, Hadron Industries.
-# Carthage is free software; you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License version 3
-# as published by the Free Software Foundation. It is distributed
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the file
-# LICENSE for details.
-
-
 import asyncio
 from carthage import *
 from carthage import podman, libvirt
 from carthage.libvirt.images import guestfs_container_to_vm, DebianBaseContainer
 from carthage.modeling import *
 
+class WhsRouter(DebianBaseContainer, podman.PodmanImageModel):
+    '''Podman image for use as a router in the lab'''
+    add_provider(podman.podman_container_host, podman.LocalPodmanContainerHost)
+    override_dependencies = True
+    from carthage.libvirt.images import NoRootCustomization, CloudInitCustomization, SerialCustomization
+
+    oci_image_tag = 'localhost/whs-router'
+    base_image = 'debian:trixie'
+    oci_image_command = ['/sbin/init']
 
 class WhsBaseImage(DebianBaseContainer, podman.PodmanImageModel):
     '''Podman image for use as a basis for libvirt VMs'''
     add_provider(podman.podman_container_host, podman.LocalPodmanContainerHost)
-    override_dependencies = True # Is this needed? 
+    override_dependencies = True
     from carthage.libvirt.images import NoRootCustomization, CloudInitCustomization, SerialCustomization
 
     oci_image_tag = 'localhost/whs-base-image'
     base_image = 'debian:trixie'
+    oci_image_command = ['/sbin/init']
 
-    class install_guest_agent(FilesystemCustomization):
+    class install_prereqs(FilesystemCustomization):
         @setup_task('Create netdev group')
         async def create_netdev_group(self):
             await self.run_command('groupadd', '--system', 'netdev')
@@ -34,8 +35,14 @@ class WhsBaseImage(DebianBaseContainer, podman.PodmanImageModel):
 
         @setup_task('Install packages')
         async def install_packages(self):
-            await self.run_command('apt-get', '-y', 'install', 'openssh-server', 'systemd-resolved', 'udev', 'iputils-ping')
+            await self.run_command('apt-get', '-y', 'install', 'openssh-server', 'systemd-resolved', 'systemd-sysv', 'udev', 'iputils-ping')
         
+        @setup_task("Use stub-resolv.conf")
+        async def use_stub_resolv(self):
+            resolv_path = self.path / "etc/resolv.conf"
+            resolv_path.unlink()
+            resolv_path.symlink_to("/run/systemd/resolve/stub-resolv.conf")
+
         guest_agent = customization_task(libvirt.InstallQemuAgent)
 
 vm_image_build_lock = asyncio.Lock()
